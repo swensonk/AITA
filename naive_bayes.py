b2741c7130eb65b69d36672a9adc2a4248c99acd
ayden-barber <barbera@msoe.edu>
2025-04-09 17:54:48 -0500

    Added cross-fold validation and averaged scores. Added samples for testing

--- naive_bayes.py
+++ naive_bayes.py
@@ -1,11 +1,13 @@
 import os
 from glob import glob
+
+import nltk.data
 from nltk.tokenize import RegexpTokenizer
 from nltk.stem import WordNetLemmatizer
 import contractions
 
 from sklearn.pipeline import Pipeline
-from sklearn.model_selection import train_test_split
+from sklearn.model_selection import train_test_split, StratifiedKFold
 from sklearn.naive_bayes import MultinomialNB
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics import classification_report
@@ -13,11 +15,22 @@ from reddit_scrape import PostStore
 import random
 from collections import defaultdict
 import numpy as np
+import pandas as pd
+import sys
+
+nltk.data.path.append("nltk_data")
+#Setting up data path
+def resource_path(relative_path):
+    if hasattr(sys, '_MEIPASS'):
+        return os.path.join(sys._MEIPASS, relative_path)
+    return os.path.join(os.path.abspath("."), relative_path)
+
 
 # Load Reddit Data
 def load_posts(folder):
     data = []
-    store = PostStore('../reddit_scraper_results')
+    data_path = resource_path('data/reddit_scraper_results')
+    store = PostStore(data_path)
     for id in store.keys():
         flair, contents = store.get(id)
         if flair in {"Not the A-hole", "Asshole", "Everyone Sucks", "No A-holes here"}:
@@ -41,7 +54,7 @@ def oversample_dataset(data):
         label_buckets[label].append((post, label))
 
     max_size = max(len(bucket) for bucket in label_buckets.values())
-    
+
     balanced = []
     for label, bucket in label_buckets.items():
         if len(bucket) < max_size:
@@ -67,13 +80,13 @@ def classify_user_input(pipeline):
 
 def main():
     # Create Model
-    data_folder = '../reddit_scraper_results'
-    raw_data = load_posts(data_folder)
+    data_path = resource_path('data/reddit_scraper_results')
+    raw_data = load_posts(data_path)
     raw_data = oversample_dataset(raw_data)
     texts = [preprocess(text) for text, flair in raw_data]
     labels = [flair for _, flair in raw_data]
 
-    X_train, X_test, y_train, y_test = train_test_split(
+    X_trainval, X_test, y_trainval, y_test = train_test_split(
         texts, labels, test_size=0.2, stratify=labels, random_state=42
     )
 
@@ -82,13 +95,34 @@ def main():
         ('tfidf', TfidfVectorizer(ngram_range=(1, 2))),  # unigrams (1, 1) - unigrams & bigrams (1, 2)
         ('nb', MultinomialNB())
     ])
+    skf = StratifiedKFold(n_splits=5)
+    all_reports = []
 
-    # Train Model
-    clf_pipeline.fit(X_train, y_train)
+    X_np = np.array(X_trainval)
+    y_np = np.array(y_trainval)
+    for train_idx, val_idx in skf.split(X_np, y_np):
+        X_train, X_val = X_np[train_idx], X_np[val_idx]
+        y_train, y_val = y_np[train_idx], y_np[val_idx]
 
-    # Evaluate Model
-    y_pred = clf_pipeline.predict(X_test)
-    print(classification_report(y_test, y_pred))
+        clf_pipeline.fit(X_train, y_train)
+        y_pred = clf_pipeline.predict(X_val)
+
+        report = classification_report(y_val, y_pred, output_dict=True)
+        all_reports.append(report)
+
+    df_reports = pd.DataFrame(all_reports)
+
+    averages = {}
+
+    for col in df_reports.columns:
+        # Convert the list of dicts into a DataFrame
+        metrics_df = pd.DataFrame(df_reports[col].tolist())
+        # Calculate the mean for each metric
+        averages[col] = metrics_df.mean()
+
+
+    averages_df = pd.DataFrame(averages).T
+    print(averages_df)
 
     classify_user_input(clf_pipeline)
 
